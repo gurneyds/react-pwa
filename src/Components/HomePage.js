e0a692355d48ba86131a40a584130fa17ce844a9
David Gurney <gurneyds@familysearch.org>
2021-04-06 17:14:35 -0600

    Detect cached data

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import OnlineStatus from './OnlineStatus'
 import './HomePage.css'
+import getCachedData from './getCachedData'
 
 export default function HomePage() {
+  const [offlineTeamData, setOfflineTeamData] = useState(() => false)
+
+  useEffect(() => {
+    async function getOfflineData() {
+      console.log('navigator.onLine=', navigator.onLine)
+
+      if (!navigator.onLine) {
+        console.log('detected offline status - looking to see if we have cache')
+        const cachedTeamData = await getCachedData('api-data', 'http://localhost:8080/people')
+        console.log('cachedTeamData=', cachedTeamData)
+        setOfflineTeamData(cachedTeamData)
+      }
+    }
+
+    getOfflineData()
+
+    window.addEventListener('online', getOfflineData)
+    window.addEventListener('offline', getOfflineData)
+
+    return () => {
+      window.removeEventListener('online', getOfflineData)
+      window.removeEventListener('offline', getOfflineData)
+    }
+  }, [])
+
   return <div>
     <OnlineStatus />
     <h1 className="selectApplication">
@@ -11,8 +37,16 @@ export default function HomePage() {
     </h1>
     <div className="applications">
       <div className="app">
-        <Link to="/team">Team Members</Link>
-        <img className="appImage" src="./groupOfPeople.jpeg" alt="Group of people" />
+        {(navigator.onLine || offlineTeamData) &&
+          <>
+            <Link to="/team">Team Members</Link>
+            <img className="appImage" src="./groupOfPeople.jpeg" alt="Group of people" />
+          </>
+        }
+
+        {!navigator.onLine && !offlineTeamData &&
+          <span>Team data not available offline until it has been loaded 1 time.</span>
+        }
       </div>
 
       <div className="app">
---
+++ src/Components/getCachedData.js
@@ -0,0 +1,11 @@
+// Get data from the cache.
+export default async function getCachedData(cacheName, url) {
+  const cacheStorage = await caches.open(cacheName);
+  const cachedResponse = await cacheStorage.match(url);
+
+  if (!cachedResponse || !cachedResponse.ok) {
+    return false;
+  }
+
+  return await cachedResponse.json();
+}
